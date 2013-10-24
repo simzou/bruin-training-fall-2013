6cf404abdcfd564e1d9b8be6444ee59ea8a2f3f2
Simon Zou <simon.zou@gmail.com>
2013-10-24 01:45:00 -0700

    Added bar charts

--- contributions/views.py
+++ contributions/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.db.models import Sum, Count
 from django.views.generic import TemplateView
@@ -46,6 +47,24 @@ class IndexView(TemplateView):
         contributors = Contribution.objects.values('clean_name')\
                     .annotate(contribs=Sum('amount')).order_by('-contribs')
 
+        states = Contribution.objects.values('state')\
+                    .annotate(contribs=Sum('amount')).order_by('-contribs')
+
+        state_amounts = []
+        for s in states[0:25]:
+            state_amounts.append({
+                'state': str(s['state']),
+                'contribs': s['contribs']
+            })
+
+        contributor_list = []
+        for c in contributors[0:10]:
+            contributor_list.append({
+                'name': c['clean_name'],
+                'contribs': c['contribs']
+            })
+        context['state_amounts'] = state_amounts
+        context['contributor_list'] = contributor_list
         context['contributors'] = enumerate(contributors[0:10], start=1)
         context['max_value'] = max(all_vals)
         context['summary_data'] = data
